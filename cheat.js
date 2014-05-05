8aeeff72ed4034dbcc5709fe8b715ece4f2c330e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-05 01:27:07 -0700

    Added some comments

--- cheat.js
+++ cheat.js
@@ -1,5 +1,10 @@
-var jqueryhtml = '<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>';
+/**
+ * @author Rukmal Weerawarana
+ *
+ * @description Short JS script to cheat on the Wikipedia Game (http://thewikigame.com/)
+ */
 
+var jqueryhtml = '<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>';
 document.getElementsByTagName('body').appendChild(jqueryhtml);
 
 var outerhtml = $('#wiki').contents();
