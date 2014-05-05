a01c2b69ed2fc29a855ea8aa2333c8dec92cff9c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-05 16:55:13 -0700

    Trying bugfix to reduce 3 clicks to 2

--- cheat.js
+++ cheat.js
@@ -5,8 +5,8 @@
  */
 
 // Appending jQuery to the DOM
-var jqueryhtml = '<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>';
-document.body.innerHTML += jqueryhtml;
+// var jqueryhtml = '<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>';
+// document.body.innerHTML += jqueryhtml;
 
 // Setting the interval to check if the game has started
 var checkInterval = 500 // ms
